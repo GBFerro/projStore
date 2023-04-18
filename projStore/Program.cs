4ceac9ac2f267d0368baeccaad7cc6622ff983b1
GBFerro <giovani.ferro@usp.br>
2023-04-18 11:55:57 -0300

    Add project files.

---
+++ projStore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Channels;
+using Controllers;
+using Models;
+
+Console.WriteLine("Projeto Loja!");
+
+Store store = new Store()
+{
+    Description = "Essa é a sua 5 loja",
+    Address = "Rua dos Bobos No 666"
+};
+
+string getInformation = (new StoreController().Insert(store) ? "Inserido" : "Erro");
+Console.WriteLine(getInformation);
+
+new StoreController().GetAll().ForEach(store => Console.WriteLine(store));
